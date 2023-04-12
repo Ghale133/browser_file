a740f078a23542d346bd17546cf45b8de7b2fc93
Ghale133 <ghale.pratima10@gmail.com>
2023-04-12 09:38:07 +0545

    first commit

---
+++ browser.py
@@ -0,0 +1,123 @@
+import os
+import shutil
+import random
+import string
+
+class File_handling:
+    def __init__(self):
+        pass
+
+
+#list directory contents
+    def list_directory(self):
+        directory_path=input("enter the directory path:")
+        contents=os.listdir(directory_path)
+        for item in contents:
+            print(item)
+
+    
+
+
+#rename folder
+
+    def rename_directory(self, oldfolder, newfolder):
+        self.oldfolder = oldfolder
+        self.newfolder = newfolder
+        current_path=input("enter the current path:")
+        os.rename( current_path+oldfolder,current_path+newfolder)
+    
+
+
+
+#rename file
+    def rename_file(self,oldname, newname):
+        self.oldname = oldname
+        self.newname = newname
+        current_path=input("enter the current_path")
+        os.rename(current_path+oldname,current_path+newname)
+
+    
+
+#copy file
+    def copy_files(self,source,destination):
+        self.source= source
+        self.destination=destination
+        shutil.copyfile(source,destination)
+
+    
+
+#copy folder
+
+    def copy_folder(self,source,destination):
+        self.source=source
+        self.destination=destination
+        shutil.copytree(source,destination)
+
+    
+
+#create empty file
+    def empty_file(self):
+        filename = input("enter the filename")
+        current_path = os.getcwd()
+        with open(os.path.join(current_path,filename), 'w') as f:
+            pass
+        print("file is created")
+
+    
+
+#create file from random text
+    def random_text_file(self):
+        current_path=os.getcwd()
+        filename=input("enter the filename:")
+        length=int(input("enter the length of random text:"))
+        try:
+            with open(os.path.join(current_path,filename),'w') as f:
+                f.write(''.join(random.choices(string.ascii_lowercase+string.digits, k=length)))
+                print("file is created")
+        except FileNotFoundError:
+            print("Directory not found")
+
+
+    
+
+#view contents of a file
+    def view_file(self):
+        with open("food.txt","r") as f:
+            content=f.read()
+            print(content)
+
+    def move_folder(self,source, destination):
+        self.source=source
+        self.destination=destination
+        shutil.move(source, destination)
+
+
+    
+    def move_file(self,source_path, destination_path):
+        self.source=source_path
+        self.destination=destination_path
+        os.rename(source_path, destination_path)
+ 
+
+    
+
+#delete file
+    def delete_file(self):
+        file=input("enter the file to delete:")
+        os.remove(file)
+    
+
+#delete folder
+    def delete_folder(self):
+        fold=input("enter the path")
+        os.rmdir(fold)
+    
+
+#hide a folder
+    def hide_folder(self,path,foldername):
+        self.path=path
+        self.foldername=foldername
+        pathfolder=os.path.join(path,foldername)
+        os.rename(pathfolder,os.path.join(path,"."+foldername))
+    
+
---
+++ main.py
@@ -0,0 +1,55 @@
+from browser import *
+def main():
+    obj=File_handling()
+    while True:
+        print("Available options:")
+        print("1. List directory contents:")
+        print("2. Rename directory:")
+        print("3. Rename file:")
+        print("4. Copy file:")
+        print("5. copy folder:")
+        print("6. create a empty file:")
+        print("7. create a file from Random text file:")
+        print("8. view file")
+        print("9. move folder")
+        print("10. Move file")
+        print("11. Delete file:")
+        print("12. Delete folder:")
+        print("13. hide folder:")
+        print("q . Quit")
+
+        choice=input("Enter your choice: ")
+        if choice=="1":
+            obj.list_directory()
+        elif choice=="2":
+            obj.rename_directory(input("enter the old folder name:"),input("enter the newfolder name:"))
+        elif choice=="3":
+            obj.rename_file(input("enter the old file name:"),input("enter the new file name:"))
+        elif choice=="4":
+            obj.copy_files(input("enter the source location with name:"),input("enter the destination location:"))
+        elif choice=="5":
+            obj.copy_folder(input("enter the source location with name:"),input("enter the destination location:"))
+        elif choice=="6":
+            obj.empty_file()
+        elif choice=="7":
+            obj.random_text_file()
+        elif choice=="8":
+            obj.view_file()
+        elif choice=="9":
+            obj.move_folder(input("enter the souce folder:"),input("enter the destination location:"))
+        elif choice=="10":
+            obj.move_file(input("enter the filename to move:"),input("enter the destination location:"))
+        elif choice=="11":
+            obj.delete_file()
+        elif choice=="12":
+            obj.delete_folder()
+        elif choice=="13":
+            obj.hide_folder(input("enter the folder path"),input("enter the folder name:"))
+        elif choice=="q":
+            break
+            
+main()
+# obj.list_directory()
+# obj.rename_directory()
+
+
